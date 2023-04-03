50659cd7f9aa063767b54d32510f8f2af180e9e8
Claudinei José Santos <claudineibitta@gmail.com>
2023-04-03 00:27:02 -0300

    refactor: insercoes de configuracoes nas rotas

--- src/routes/authorsRoutes.js
+++ src/routes/authorsRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authorsController from "../controllers/authorsController.js";
+import page from "../middleware/page.js";
 
 const router = express.Router();
 
 router
-    .get("/authors", authorsController.getAuthors)
+    .get("/authors", authorsController.getAuthors, page)
     .get("/authors/:id", authorsController.getAuthorById)
     .post("/authors", authorsController.registerAuthor)
     .put("/authors/:id", authorsController.updatedAuthor)
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import booksController from "../controllers/booksController.js";
+import page from "../middleware/page.js";
 
 const router = express.Router();
 
 router
-    .get("/books", booksController.getBooks)
-    .get("/books/get", booksController.getBookByPublisher)
+    .get("/books", booksController.getBooks, page)
+    .get("/books/get", booksController.getBookByFilter, page)
     .get("/books/:id", booksController.getBookById)
     .post("/books", booksController.registerBook)
     .put("/books/:id", booksController.updatedBook)
